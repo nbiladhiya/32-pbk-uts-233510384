623f5b8ccb546cb36d86557321fc6ac0d25bf056
nbiladhiya <dwinabiladhiyaulhaq@student.uir.ac.id>
2025-05-01 12:09:52 +0700

    add task

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <script setup>
+import { ref } from 'vue';
 
+const tasks = ref([]);
+const newTask = ref('');
+
+const addTask = () => {
+  if (newTask.value.trim()) {
+    tasks.value.push({
+      id: tasks.value.length + 1,
+      text: newTask.value, 
+      completed: false 
+    });
+    console.log(tasks.value);
+    newTask.value = '';
+  }
+};
 </script>
 
 <template>
-  
+  <h1>Task Manager</h1>
+  <input type="text" v-model="newTask" @keyup.enter="addTask" placeholder="Add a new task" />
+  <button @click="addTask">Add</button>
 </template>
 
 <style scoped>
